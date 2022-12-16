08844789d343e34c7f10f1f0b992c7478b9017be
Nguyen Vo Minh Duy <thavananh@gmail.com>
2022-12-16 15:21:58 +0700

    Maybe_Final

---
+++ js/autoslide.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+    var counter = 1;
+    setInterval(function(){
+      document.getElementById('radio' + counter).checked = true;
+      counter++;
+      if(counter > 4){
+        counter = 1;
+      }
+    }, 5000);
+  </script>
